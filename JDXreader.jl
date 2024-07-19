919730a8555b62981a2bc33af634e283b6b5fd70
Manarom <97886309+Manarom@users.noreply.github.com>
2024-07-19 16:42:46 +0300

    Update JDXreader.jl
    DEscription added

--- JDXreader.jl
+++ JDXreader.jl
@@ -1,9 +1,40 @@
-using DelimitedFiles #,CSV
+
+# module to read JCAMP-DX=4.24 file formats
 
 module JDXreader
-# reader for JDXformat files
-    export JDXfile,read!
-    using ..DelimitedFiles
+"""
+JCAMP file example: 
+            ##TITLE=1
+            ##JCAMP-DX=4.24
+            ##DATATYPE=INFRARED SPECTRUM
+            ##DATE=2021/10/17
+            ##TIME=11:30
+            ##XUNITS=1/CM
+            ##YUNITS=TRANSMITTANCE
+            ##XFACTOR=1
+            ##YFACTOR=0.00699183
+            ##FIRSTX=0
+            ##LASTX=15801.4671743
+            ##FIRSTY=11746.3412893072
+            ##MAXX=15801.4671743
+            ##MINX=0
+            ##MAXY=3.75371e+006
+            ##MINY=4040.25
+            ##NPOINTS=16384
+            ##XYDATA=(X++(Y..Y))
+            0 1680010 821286 2148133 1505245 1537124 1367661 1147725 1134981
+            7.71603 1166853 1213186 1029828 1067595 1135904 1195128 1157134 1150556
+            15.4321 1266743 1164401 1014224 1022338 999780 1138781 1208950 1161258
+            .
+            .
+            ##END
+
+Header lines start with ##
+Data start after headers lines
+
+"""
+    export JDXfile,read!,read_jdx_file
+    #using ..DelimitedFiles
     mutable struct JDXfile
         # Main struct, prepares loads file name, parses data and data headers
         file_name::String
@@ -11,6 +42,10 @@ module JDXreader
         x_data::Vector{Float64}
         y_data::Vector{Float64}
         is_violated_flag::Vector{Bool}
+        """
+        JDXfile()
+        JDXreader obj constructor, creates empty object with no data
+    """
         JDXfile()=begin
             new("",
                 Dict{String,Union{String,Number}}(),
@@ -19,9 +54,24 @@ module JDXreader
                 Vector{Bool}())
         end
     end
+    """
+    read_jdx_file(file_name::String)
+
+    Read JCAMP format file file_name - full file name,
+    returns named tuple with fields :
+           x - coordinate (wavelength, wavenumber or other)
+           y - data
+           headers - dictionary in "String => value" format with 
+           headers values  
+"""
     function read_jdx_file(file_name::String)
         return file_name |> JDXfile |> read!
     end
+    """
+    JDXfile(file_name::String)
+
+Creates JDXreader obj from full file name 
+"""
     function JDXfile(file_name::String) # external constructor
         if !isfile(file_name)
             error("Input filename must be the fullfile name to the existing file")
@@ -59,24 +109,6 @@ module JDXreader
         return data_intermediate[1]
     end
     function generateXvector!(jdx::JDXfile)
-            #xydata.xValues = new double[xydata.dataspec.NPoints];// инициализируем массивы, который будут выдаваться в качестве результата чтения файла
-            #xydata.yValues = new double[xydata.dataspec.NPoints];
-            #double step = ((xydata.dataspec.LastX - xydata.dataspec.FirstX)/xydata.dataspec.XFactor)/(xydata.dataspec.NPoints-1);
-            ##DATE=2021/10/06
-            ##TIME=13:42
-            ##XUNITS=1/CM
-            ##YUNITS=TRANSMITTANCE
-            ##XFACTOR=1
-            ##YFACTOR=0.00702876
-            ##FIRSTX=0
-            ##LASTX=15794.7156470938
-            ##FIRSTY=8921.3875152695
-            ##MAXX=15794.7156470938
-            ##MINX=0
-            ##MAXY=3.77354e+006
-            ##MINY=1894.21
-            ##NPOINTS=2048
-            ##XYDATA=(X++(Y..Y))
             point_number = jdx.data_headers["NPOINTS"]
             starting_X = haskey(jdx.data_headers,"FIRSTX") ? jdx.data_headers["FIRSTX"] : 0.0
             if haskey(jdx.data_headers,"DELTAX")
@@ -91,7 +123,13 @@ module JDXreader
             resize!(jdx.x_data,point_number)
             jdx.x_data .= [starting_X + i*step_value for i in 0:point_number-1]
     end
-    function read!(jdx::JDXfile)
+    """
+    read!(jdx::JDXfile)
+
+    fills precreated JDXfile
+
+"""
+function read!(jdx::JDXfile)
         if !isfile(jdx.file_name)
             return nothing
         end
@@ -117,10 +155,9 @@ module JDXreader
                 generateXvector!(jdx)
                 resize!(jdx.y_data,total_point_number)
                 for i in 2:data_lines_number
-                    ln = readline(io_file)
-                    x_point = addYline!(jdx,ln,number_of_y_point_per_chunk,i)
+                    x_point = addYline!(jdx,readline(io_file),number_of_y_point_per_chunk,i)
                 end
-            else number_of_y_point_per_chunk==1
+            else number_of_y_point_per_chunk==1 # file with two columns like CSV
                 total_point_number = data_lines_number
                 resize!(jdx.y_data,total_point_number)
                 jdx.x_data = similar(jdx.y_data)
